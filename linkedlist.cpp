b16ebf5f00f5121f585218a549681ac1cc1cb50c
Madhukar K Rupakumar <madhukar.rupakumar@hpe.com>
2023-11-15 22:56:20 +0530

    committing defn of LL

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,3 +1,12 @@
-class LinkedList {
-    
+#include <cstddef>
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+        
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = NULL;
+        }
+
 };
