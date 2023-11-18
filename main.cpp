944a00df7c722f02538ada0f5d9feefed7396771
Madhukar K Rupakumar <madhukar.rupakumar@hpe.com>
2023-11-18 18:47:07 +0530

    update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
-#include "stdio.h"
+#include "linkedlist.h"
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello, world!" << std::endl;
-    
+// Driver Code 
+int main() 
+{ 
+	Linkedlist list; 
+
+	// Inserting nodes 
+	list.insertNode(1); 
+	list.insertNode(2); 
+	list.insertNode(3); 
+	list.insertNode(4); 
+
+	cout << "Elements of the list are: "; 
+
+	// Print the list 
+	list.printList(); 
+	cout << endl; 
+
+	// Delete node at position 2. 
+	list.deleteNode(2); 
+
+	cout << "Elements of the list are: "; 
+	list.printList(); 
+	cout << endl; 
+	return 0; 
 }
